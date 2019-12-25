be4e788c4104caed4e6315aba873b520ed9e24ed
Josef Lintz <yosi1.lints@gmail.com>
2019-12-25 16:22:54 +0200

    + Added DI class

---
+++ BinaryTreeTest/DI/DI.cs
@@ -0,0 +1,15 @@
+﻿namespace BinaryTreeTest
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DI
+    {
+        public static Tree Tree { get; set; }
+
+        public static List<NodeItemViewModel> NodeItems { get; set; }
+
+    };
+};
