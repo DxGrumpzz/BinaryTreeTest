663e671cd8c5efdfe44a158967a28d039805b3e9
Josef Lintz <yosi1.lints@gmail.com>
2019-12-25 14:53:10 +0200

    + Added NodeItemView

---
+++ BinaryTreeTest/Views/NodeItemView.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace BinaryTreeTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for NodeItemView.xaml
+    /// </summary>
+    public partial class NodeItemView : UserControl
+    {
+
+        public NodeItemView()
+        {
+            InitializeComponent();
+        }
+
+    };
+};
