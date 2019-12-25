e99bbeb3803d7c22b5281df67e31584fabfd2fee
Josef Lintz <yosi1.lints@gmail.com>
2019-12-25 16:48:32 +0200

    + Added BaseViewModel

---
+++ BinaryTreeTest/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BinaryTreeTest
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName()]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    };
+};
