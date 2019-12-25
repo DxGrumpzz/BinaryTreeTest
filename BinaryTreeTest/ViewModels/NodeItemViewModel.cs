4e0e9ae0c8a54da93e9989291ef1fccd115ab45d
Josef Lintz <yosi1.lints@gmail.com>
2019-12-25 14:43:42 +0200

    + Added node item viewmodel

---
+++ BinaryTreeTest/ViewModels/NodeItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BinaryTreeTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class NodeItemViewModel
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Node Node { get; set; }
+
+
+
+    };
+};
