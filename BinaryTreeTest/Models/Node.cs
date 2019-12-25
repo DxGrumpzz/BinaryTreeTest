3be4c9cf9d09167433f2d30171bdd79a098e1fd4
Josef Lintz <yosi1.lints@gmail.com>
2019-12-25 14:43:12 +0200

    * Moved models into models folder

---
+++ BinaryTreeTest/Models/Node.cs
@@ -0,0 +1,29 @@
+﻿namespace BinaryTreeTest
+{
+    using System.Diagnostics;
+
+    /// <summary>
+    /// A node, Holds some data and points to other nodes to it's left and right
+    /// </summary>
+    [DebuggerDisplay("{DebuggerDisplay(), nq}")]
+    public class Node
+    {
+        /// <summary>
+        /// The ID number of this node
+        /// </summary>
+        public int NodeID { get; set; }
+
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+
+
+        private string DebuggerDisplay()
+        {
+            var leftNodeString = LeftNode != null ? LeftNode.NodeID.ToString() : "null";
+            var rightNodeString = RightNode != null ? RightNode.NodeID.ToString() : "null";
+
+            return $"ID: {NodeID}, L: {leftNodeString}, R: {rightNodeString}";
+        }
+
+    };
+};
---
+++ BinaryTreeTest/Models/NodePosition.cs
@@ -0,0 +1,23 @@
+﻿namespace BinaryTreeTest
+{
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Holds a node with it's position relative to canvas
+    /// </summary>
+    [DebuggerDisplay("{DebuggerDisplay(), nq}")]
+    public class NodePosition
+    {
+        public Node Node { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+        private string DebuggerDisplay()
+        {
+            return $"ID: {Node.NodeID}, X: {X}, Y: {Y}";
+        }
+
+    };
+};
